5cfdf461460e8e9ac5ee8d3c078584af526903a4
Vijay Jangale <vijay.j@in-mac-020.local>
2013-03-21 11:38:09 +0530

    Cr By:Chetan

---
+++ testTPT/DPTest1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  DPTest1ViewController.h
+//  testTPT
+//
+//  Created by Vijay Jangale on 21/03/13.
+//  Copyright (c) 2013 Dinesh Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPTest1ViewController : UIViewController
+
+@end
---
+++ testTPT/DPTest1ViewController.m
@@ -0,0 +1,38 @@
+//
+//  DPTest1ViewController.m
+//  testTPT
+//
+//  Created by Vijay Jangale on 21/03/13.
+//  Copyright (c) 2013 Dinesh Software. All rights reserved.
+//
+
+#import "DPTest1ViewController.h"
+
+@interface DPTest1ViewController ()
+
+@end
+
+@implementation DPTest1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
